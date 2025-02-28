bd34af2f892cb1c308c5fd1a8e06d5c0156aef27
William Chien <william.chien@mail.mcgill.ca>
2025-02-28 10:40:17 -0600

    submission

--- dateTimeFunction/src/main/java/com/example/DateTimeFunctions.java
+++ dateTimeFunction/src/main/java/com/example/DateTimeFunctions.java
@@ -22,7 +22,7 @@ public class DateTimeFunctions {
 	 */
 	public static LocalDate getTodaysDate() {
 		//YOUR CODE STARTS HERE
-		return null;
+		return LocalDate.now();
 		//YOUR CODE ENDS HERE
 
 	}
@@ -33,7 +33,7 @@ public class DateTimeFunctions {
 	 */
 	public static LocalDate getLaterDatebyDays(LocalDate date, int x) {
 		//YOUR CODE STARTS HERE
-		return null;
+		return date.plusDays(x);
 		//YOUR CODE ENDS HERE
 
 	}
@@ -44,7 +44,7 @@ public class DateTimeFunctions {
 	 */
 	public static LocalDate getPreviousDatebyWeeks(LocalDate date, int x) {
 		//YOUR CODE STARTS HERE
-		return null;
+		return date.minusWeeks(x);
 		//YOUR CODE ENDS HERE
 
 	}
@@ -56,7 +56,8 @@ public class DateTimeFunctions {
 	 */
 	public static String getTimeDifference(LocalDate date1, LocalDate date2) {
 		//YOUR CODE STARTS HERE
-		return null;
+		Period diff = date1.until(date2);
+		return "Years-" + diff.getYears() + ":Months-" + diff.getMonths() + ":Days-" + diff.getDays();
 		//YOUR CODE ENDS HERE
 
 	}
